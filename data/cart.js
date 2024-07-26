d18ce4247d10c6ffb50a99e05241fe47dcf20e25
ebraheemalimahrous <ebraheemalimahrous000@gmail.com>
2024-07-26 19:45:33 +0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart = [{
     // Normalizing the data.
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
@@ -6,6 +9,7 @@ export let cart = [{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+}
 
 
 export function addToCart(productId){
@@ -23,6 +27,12 @@ export function addToCart(productId){
         quantity: 1
     })
     }
+    saveToStorage()
+}
+
+// localStorage
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart))
 }
 
 
@@ -41,5 +51,5 @@ export function removeFromCart(productId){
     })
 
     cart = newCart;
-
+    saveToStorage()
 }
