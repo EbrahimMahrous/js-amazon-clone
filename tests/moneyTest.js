adbd8534b9199358a08a4f6eeda9f7ae57bc4f0e
ebraheemalimahrous <ebraheemalimahrous000@gmail.com>
2024-07-28 19:56:21 +0300

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,55 @@
+
+// Easiest way to test:
+// Open the website and try out the code.
+
+// Disadvantages of Manual Testing
+// 1. Hard to test every situation.
+// 2. Hard to re-test.
+
+
+// Automated Testing = using code to test code.
+
+
+
+// Situation = test Case
+
+
+// How many test cases should we have?
+
+// Two Types of Test Cases: 
+// 1. Basic test cases ==> tests if the code is working.
+// 2. Edge cases ==> test with values that are tricky.
+
+
+// Try to test something different in each test case.
+
+
+
+import formatCurrency from "../scripts/utils/money.js";
+
+// Group related tests together = test suite.
+console.log('test suite: formatCurrency');
+
+console.log('converts cents into dollars');
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cent');
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
