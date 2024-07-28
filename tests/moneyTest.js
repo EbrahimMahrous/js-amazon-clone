3f906663c21d9053122a6f1b3ca8c0ca69a82006
ebraheemalimahrous <ebraheemalimahrous000@gmail.com>
2024-07-29 02:47:37 +0300

    Add jasmine testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -53,3 +53,14 @@ if (formatCurrency(2000.5) === '20.01') {
 } else {
   console.log('failed');
 }
+
+// Testing Framework = external library that helps us write tests easier.
+// Most testing framework are similar Other testing framework ==> Jest(for ReactJS) + MochaJS
+// Popular test framework is Jasmine ==> 
+
+
+
+
+// 1. Create test suite.
+// 2. Create tests.
+// 3. Create values and display result.
